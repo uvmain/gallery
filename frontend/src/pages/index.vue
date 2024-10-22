8333e7d5dcea415777f3f28981431c481f5c296a
Ian Baron <uv.fragm@gmail.com>
2024-10-22 14:03:11 +0100

    offset and limit are reactive

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from 'vue'
 
-const slugs = ref()
+const slugs = ref<string[] | undefined>([])
 const showModal = ref(false)
 const selectedImage = ref<string | null>(null)
 
+const limit = ref(30)
+const offset = ref(0)
+
 async function getSlugs() {
   try {
-    fetch('http://localhost:8080/api/slugs?offset=0&limit=30')
+    fetch(`http://localhost:8080/api/slugs?offset=${offset.value}&limit=${limit.value}`)
       .then(response => response.json())
-      .then(json => slugs.value = json)
+      .then((json) => {
+        const jsonData = json as string[]
+        offset.value += jsonData.length
+        slugs.value = jsonData
+      })
   }
   catch (error) {
     console.error('Failed to fetch thumbnails:', error)
@@ -45,13 +52,13 @@ onBeforeMount(async () => {
       <div v-for="(slug, index) in slugs" :key="index">
         <img :src="getThumbnailPath(slug)" :alt="slug" class="h-20vh min-h-200px cursor-pointer" @click="openModal(getImagePath(slug))">
       </div>
-      <div v-if="showModal" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-        <div class="relative rounded-lg bg-white p-4">
-          <button class="absolute right-2 top-2 size-6 rounded-lg bg-red-500 text-white" @click="closeModal">
-            X
-          </button>
-          <img v-if="selectedImage" :src="selectedImage" alt="Selected Image" class="max-h-screen max-w-full">
-        </div>
+    </div>
+    <div v-if="showModal" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+      <div class="relative rounded-lg bg-white p-4">
+        <button class="absolute right-2 top-2 size-6 rounded-lg bg-red-500 text-white" @click="closeModal">
+          X
+        </button>
+        <img v-if="selectedImage" :src="selectedImage" alt="Selected Image" class="max-h-screen max-w-full">
       </div>
     </div>
   </div>
