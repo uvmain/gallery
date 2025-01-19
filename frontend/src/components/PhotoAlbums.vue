addbc0c648cec5e9bd0702ff636f06acebec8395
Ian Baron <uv.fragm@gmail.com>
2025-01-19 12:44:23 +0000

    fix ratio for panoramic images

--- frontend/src/components/PhotoAlbums.vue
+++ frontend/src/components/PhotoAlbums.vue
@@ -37,11 +37,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="mt-4 w-full flex flex-col gap-4 border-1 border-gray-400 border-solid p-4">
+  <div class="mt-4 max-w-100 flex flex-col gap-4 border-1 border-gray-400 border-solid p-4">
     <div class="text-left text-lg">
       This photo is in {{ imageAlbums.length }} {{ imageAlbums.length === 1 ? 'album' : 'albums' }}
     </div>
-    <div class="grid grid-cols-2 gap-4 lg:grid-cols-4 md:grid-cols-3">
+    <div v-if="inEditingMode || imageAlbums.length" class="grid grid-cols-2 gap-4 lg:grid-cols-4 md:grid-cols-3">
       <div v-for="(album, index) in imageAlbums" :key="index">
         <AlbumCoverSmall :album="album" />
       </div>
--- frontend/src/pages/[imageSlug].vue
+++ frontend/src/pages/[imageSlug].vue
@@ -193,7 +193,9 @@ onBeforeMount(async () => {
       </div>
     </Header>
     <div id="main" class="flex flex-col justify-center gap-6 p-6 lg:flex-row">
-      <img v-if="imageSource" :src="imageSource" class="max-h-80vh max-w-70vw border-6 border-white border-solid dark:border-neutral-500" />
+      <div class="flex items-center align-middle">
+        <img v-if="imageSource" :src="imageSource" class="border-6 border-white border-solid dark:border-neutral-500 object-contain max-h-80vh max-w-70vw" />
+      </div>
       <div v-if="metadata" class="flex flex-col gap-4 p-6 text-sm lg:max-w-1/3">
         <div class="font-semibold lg:text-lg">
           <div v-if="inEditingMode">
