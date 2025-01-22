6c9ff06a10b03df9e7804ea6aa2e1ad0753cd6f7
Ian Baron <uv.fragm@gmail.com>
2025-01-22 14:07:26 +0000

    can now add/delete tags

--- frontend/src/components/Tag.vue
+++ frontend/src/components/Tag.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type Dialog from './Dialog.vue'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const props = defineProps({
@@ -6,16 +8,47 @@ const props = defineProps({
   inEditingMode: { type: Boolean, default: false },
 })
 
-const router = useRouter()
+const emits = defineEmits(['delete'])
 
-function navigateToTag() {
-  const tagPath = `/tags/${props.tag}`
-  router.push(tagPath)
+const router = useRouter()
+const deleteDialog = ref<typeof Dialog>()
+
+function onClick() {
+  if (!props.inEditingMode) {
+    const tagPath = `/tags/${props.tag}`
+    router.push(tagPath)
+  }
+  else {
+    deleteDialog.value?.show()
+  }
+}
+
+function hideDeleteDialog() {
+  deleteDialog.value?.hide()
 }
 </script>
 
 <template>
-  <div class="text-nowrap tag" @click="navigateToTag">
-    {{ tag }}
+  <div>
+    <div class="text-nowrap tag" :class="inEditingMode ? 'hover:bg-red hover:dark:bg-red' : ''" @click="onClick">
+      {{ tag }}
+    </div>
+    <Dialog ref="deleteDialog" :close-button="false" class="modal">
+      <div class="p-6">
+        <div class="flex flex-col items-center gap-6">
+          <p>
+            Delete Tag: {{ tag }}
+          </p>
+          <div class="flex justify-center gap-4">
+            <button aria-label="cancel" class="button" @click="hideDeleteDialog()">
+              Cancel
+            </button>
+            <button aria-label="delete" class="button" @click="emits('delete', tag)">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </Dialog>
   </div>
 </template>
--- frontend/src/components/Tags.vue
+++ frontend/src/components/Tags.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type Dialog from '../components/Dialog.vue'
 import { onMounted, ref, watch } from 'vue'
 import { backendFetchRequest } from '../composables/fetchFromBackend'
 
@@ -10,6 +11,8 @@ const props = defineProps({
 defineExpose({ getTags })
 
 const tags = ref<string[]>([])
+const newTagString = ref<string>()
+const addDialog = ref<typeof Dialog>()
 
 async function getTags() {
   tags.value = []
@@ -17,8 +20,50 @@ async function getTags() {
   tags.value = await tagsRequest.json() || []
 }
 
-async function addTag() {
-  return null
+function showAddDialog() {
+  addDialog.value?.show()
+}
+
+function hideAddDialog() {
+  addDialog.value?.hide()
+}
+
+async function confirmAddTag() {
+  const newTag = {
+    Tag: newTagString.value,
+    ImageSlugs: [props.imageSlug],
+  }
+  const options = {
+    body: JSON.stringify(newTag),
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  }
+  const response = await backendFetchRequest('tags', options)
+
+  hideAddDialog()
+
+  if (response.status === 200) {
+    await getTags()
+  }
+}
+
+async function deleteTag(tag: string) {
+  const newTag = {
+    Tag: tag,
+    ImageSlug: props.imageSlug,
+  }
+  const options = {
+    body: JSON.stringify(newTag),
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  }
+  const response = await backendFetchRequest('tags', options)
+
+  hideAddDialog()
+
+  if (response.status === 200) {
+    await getTags()
+  }
 }
 
 watch(
@@ -40,13 +85,32 @@ onMounted(() => {
     </div>
     <div class="flex flex-wrap gap-4">
       <div v-for="(tag, index) in tags" :key="index">
-        <Tag :tag="tag" />
+        <Tag :tag="tag" :in-editing-mode="inEditingMode" @delete="deleteTag(tag)" />
       </div>
       <div v-if="inEditingMode">
-        <div class="hover:cursor-pointer" @click="addTag()">
+        <div class="hover:cursor-pointer" @click="showAddDialog()">
           <icon-tabler-square-plus class="text-2xl" />
         </div>
       </div>
     </div>
+    <Dialog ref="addDialog" :close-button="false" class="modal">
+      <div class="p-6">
+        <div class="flex flex-col items-center gap-6">
+          <icon-tabler-library-plus class="text-4xl text-green" />
+          <div class="flex flex-row items-center gap-2">
+            <label for="newtag">New Tag:</label>
+            <input id="newtag" v-model="newTagString" type="text" name="newtag">
+          </div>
+          <div class="flex justify-center gap-4">
+            <button aria-label="cancel" class="button" @click="hideAddDialog()">
+              Cancel
+            </button>
+            <button aria-label="delete" class="button" @click="confirmAddTag()">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </Dialog>
   </div>
 </template>
